b8108a44a829247f8504567aa279f5ee84a1695a
ale-cristofori <compatt84@gmail.com>
2020-05-28 08:48:44 +0100

    introduced dots  on lines

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { scaleLinear, max, line, select, path } from 'd3';
+import { scaleLinear, max, line, select, path, selectAll } from 'd3';
 import { lineChartData } from '../../testData';
 
 const lineTileTextStyle = {
@@ -8,34 +8,56 @@ const lineTileTextStyle = {
     fontWeight: "bold"
 }
 
-const amimateLine = (currentLine, newLinePath, lineColour) => {
+const amimateLine = (margin, currentLine, selectedData, lineColour) => {
+    const width = 500 - margin.left - margin.right;
+    const height = 150 - margin.top - margin.bottom;
+    const xScale = scaleLinear()
+    .domain([0, selectedData.length - 1])
+    .range([0, width]);
+
+    const yScale = scaleLinear()
+        .domain([0, max(selectedData, d => d.measure)])
+        .range([height, 0]);
+
+    const lineDefinition = line()
+            .x((d, i) => xScale(i) * 0.10)
+            .y(d => yScale(d.measure) * 0.10);
+    const points = selectedData.map( (item, index) => { return {x: (xScale(index) * 0.10), y: (yScale(item.measure) * 0.10)}})
+    const newLinePath = lineDefinition(selectedData);
     currentLine.transition()
         .duration(650)
         .attr("d", newLinePath)
-        .attr("stroke", lineColour);
+        .attr("stroke", lineColour)
+    
+    points.map((point) => {
+        select(".line")
+        .append("circle")
+        .attr("cx", point.x)
+        .attr("cy", point.y)
+        .attr("r", 0.5)
+        .attr("fill", "red");
+    });
 };
 
 const Dot = (props) => {
 
-    const {datum, margin} = props;
-    const width = 500 - margin.left - margin.right;
-    const height = 150 - margin.top - margin.bottom;
+    const {selectedData, margin} = props;
 
-    const xScale = scaleLinear()
-    .domain([0, 1])
-    .range([0, width]);
 
-    const yScale = scaleLinear()
-        .domain([0, max(datum.measure)])
-        .range([height, 0]);
+    // const xScale = scaleLinear()
+    // .domain([0, 1])
+    // .range([0, width]);
+
+    // const yScale = scaleLinear()
+    //     .domain([0, max(selectedData.measure)])
+    //     .range([height, 0]);
         
-    const circleDefinition = line()
-            .x((d, i) => xScale(i) * 0.10)
-            .y(d => yScale(d.measure) * 0.10);
+    // const circleDefinition = line()
+    //         .x((d, i) => xScale(i) * 0.10)
+    //         .y(d => yScale(d.measure) * 0.10);
 
-    return(
-        <circle fill="blue" cx={circleDefinition.x()} cy={circleDefinition.y()}></circle>
-    )
+    const dots = selectedData.map(datum => <circle fill="blue" cx={3} cy={10} r={1}></circle>) 
+    return dots
 }
 
 const Line = (props) => {
@@ -45,31 +67,18 @@ const Line = (props) => {
     const width = 500 - margin.left - margin.right;
     const height = 150 - margin.top - margin.bottom;
 
-    const dots = selectedData.map((datum) =>
-    <Dot datum={datum} margin={margin}/>);
+    // const dots = selectedData.map((datum) =>
+    // <Dot datum={datum} margin={margin}/>);
 
     useEffect(() => {
-        const xScale = scaleLinear()
-        .domain([0, selectedData.length - 1])
-        .range([0, width]);
-    
-        const yScale = scaleLinear()
-            .domain([0, max(selectedData, d => d.measure)])
-            .range([height, 0]);
-    
-        const lineDefinition = line()
-                .x((d, i) => xScale(i) * 0.10)
-                .y(d => yScale(d.measure) * 0.10);
-        
-        const currentLine = select(lineRef.current);
-        const newLinePath = lineDefinition(selectedData);
 
-        amimateLine(currentLine, newLinePath, lineColour);
+
+        const currentLine = select(lineRef.current);
+        amimateLine(margin, currentLine, selectedData, lineColour);
     });
 
     return(
-        <g>
-            { dots }
+        <g className="line">
             <path ref={lineRef} strokeWidth="0.3" fill="none"/>
         </g>
 
@@ -85,6 +94,7 @@ const LineChart = (props) =>  {
     return(
         <g transform={`translate(${positionX}, ${positionY})`}>
             <Line margin={margin} lineColour={lineColour} selectedData={selectedData}/>
+            {/* <Dot className="dot" margin={margin} selectedData={selectedData} /> */}
             {/* <text style={lineTileTextStyle}>here the line component</text> */}
         </g>);
 
